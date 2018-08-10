f2b6fe5d898cae8a26985ec2630b00c74ed73ea2
daydevelops <adamday@daydevelops.com>
2018-08-10 14:57:43 +0000

    Switched left motor to port 3 on motor controller

---
+++ testdrive.py
@@ -0,0 +1,13 @@
+import time
+import Robot
+
+LEFT_TRIM   = 0
+RIGHT_TRIM  = 0
+
+robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
+
+robot.leftM(-100)
+time.sleep(2)
+robot.rightM(-100)
+time.sleep(2)
+robot.stop() 
