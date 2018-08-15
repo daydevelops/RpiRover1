86f08dcdf330c1f9b534b62306161487e03023e3
Adam Day <adamday@daydevelops.com>
2018-08-15 20:24:27 -0230

    added links for controller and sensor page. Added empty controller page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname));
 app.set('view engine','hbs');
 
 app.use((req,res,next) => {
@@ -19,9 +19,17 @@ app.use((req,res,next) => {
     next();
 })
 
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+})
+
 app.get('/',(req,res) => {
     res.render('index.hbs');
 });
+
+app.get('/controller',(req,res) => {
+    res.render('controller.hbs');
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
