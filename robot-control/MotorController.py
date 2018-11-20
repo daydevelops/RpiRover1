78a2d82040018454cc63356ae61ab931d748d68c
daydevelops <adamday@daydevelops.com>
2018-11-20 22:08:21 +0000

    added simulation option to arduino controller

--- robot-control/MotorController.py
+++ robot-control/MotorController.py
@@ -1,26 +1,31 @@
 # class to interface with motor controller on the arduino 
 import serial
-
+simulation = true # true if testing without real arduino connection
 
 class MotorController(object):
     def __init__(self):
-        self.ser = serial.Serial('/dev/ttyACM0',9600)
+		if (!simulation):
+			self.ser = serial.Serial('/dev/ttyACM0',9600)
 
     def stop(self):
         """Stop all movement."""
         print "telling arduino to stop"
-        self.ser.write('S;')
+		if (!simulation):
+			self.ser.write('S;')
             
     def leftM(self, speed):
         """Spin left motor at given speed"""
         print "telling Arduino to change left motor speed"
         print 'L'+str(speed)+';'
-        self.ser.write("L"+str(speed)+";")
+		if (!simulation):
+			self.ser.write("L"+str(speed)+";")
 			
     def rightM(self, speed):
         """Spin right motor at given speed"""
         print "telling Arduino to change right motor speed"
         print 'R'+str(speed)+';'
-        self.ser.write('R'+str(speed)+';')
+        
+		if (!simulation):
+			self.ser.write('R'+str(speed)+';')
 
 
